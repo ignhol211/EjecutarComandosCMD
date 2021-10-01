c6f1267924782d7f9030072483b3cb71a3a0a6de
ignhol211 <90613502+ignhol211@users.noreply.github.com>
2021-10-01 18:54:42 +0200

    Terminado

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ public class Main {
         System.out.println(hacerDirDoble());
 
     }
-
+    //Opción A: la función hacerDirDoble no imprime nada por pantalla. Obliga a devolver dos String concatenados.
+    //Opción B: la función hacerDirDoble imprime la primera parte por pantalla y llama a la función repetirDir
     public static String hacerDirDoble() {
-        String impresion="";
-        String impresion2="";
+        //String impresionA="";
+        StringBuilder impresionB1= new StringBuilder();
+        StringBuilder impresionB2= new StringBuilder();
 
         try {
             ProcessBuilder builder = new ProcessBuilder(
@@ -22,28 +24,32 @@ public class Main {
             String line;
             while ((line = r.readLine()) != null) {
 
-                impresion += line+"\n";
+                //System.out.println(line);
+
+                impresionB1.append(line).append("\n");
 
                 if(line.contains("<DIR>"))
-                    impresion2+=repetirDir(line.substring(36));
+                    //impresionA+=repetirDir(line.substring(36));
+                    impresionB2.append(repetirDir(line.substring(36)));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return impresion+impresion2;
+        //return impresionA;
+        return impresionB1 + impresionB2.toString();
     }
 
     public static String repetirDir(String nombre) throws IOException {
         String cadena;
-        String impresion2="";
+        StringBuilder impresion2= new StringBuilder();
         ProcessBuilder builder2 = new ProcessBuilder(
                 "cmd.exe", "/c", "cd C:\\"+nombre+" && dir");
         builder2.redirectErrorStream(true);
         Process p2 = builder2.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p2.getInputStream()));
         while ((cadena = r.readLine()) != null)
-            impresion2+=cadena+"\n";
-        return impresion2;
+            impresion2.append(cadena).append("\n");
+        return impresion2.toString();
     }
 }
